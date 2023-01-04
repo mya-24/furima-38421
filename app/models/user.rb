f2cf6ff30ed1c6521c8207cb30b57f09c1c7bc27
mya-24 <87232931+mya-24@users.noreply.github.com>
2023-01-04 17:39:19 +0900

    バリデーション入力、DBマイグレーションまで

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,17 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validates :nickname,             presence: true
+  validates :firstname_kanji,      presence: true
+  validates :lastname_kanji,       presence: true
+  validates :firstname_katakana,   presence: true
+  validates :lastname_katakana,    presence: true
+  validates :birthday,             presence: true
+
+  has_many :items
+  has_many :purchase_logs
+
 end
--- db/migrate/20221225103012_create_buying_logs.rb
+++
@@ -1,8 +0,0 @@
-class CreateBuyingLogs < ActiveRecord::Migration[6.0]
-  def change
-    create_table :buying_logs do |t|
-
-      t.timestamps
-    end
-  end
-end
